5f5554f4e1c6b462373b1cf72d1dbf9d3b602e60
kongdai123 <tianfuwang2021@u.northwestern.edu>
2024-06-14 12:17:28 +0200

    add nvdiffrast checkpoint

--- scripts/setup_nvdiffrast.sh
+++ scripts/setup_nvdiffrast.sh
@@ -1,6 +1,7 @@
 rm -rf ./nvdiffrast
 git clone https://github.com/NVlabs/nvdiffrast
 cd ./nvdiffrast
+git checkout c5caf7b
 PATCH_PATH=../patches/nvdiffrast.patch
 git apply "${PATCH_PATH}"
 pip install .
